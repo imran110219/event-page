5f81e648f66620553be988bc19bbbb385bd1cf3c
Sadman Sobhan <imran110219@gmail.com>
2026-03-18 16:33:06 +0600

    feat: update social links to include only Facebook for streamlined navigation

--- src/data/site.ts
+++ src/data/site.ts
@@ -7,9 +7,5 @@ export const siteConfig = {
   email: "hello@summitorganization.org",
   phone: "+1 (555) 014-7820",
   address: "Jhikargacha, Jashore, Bangladesh",
-  social: [
-    { name: "LinkedIn", url: "https://www.linkedin.com" },
-    { name: "X", url: "https://x.com" },
-    { name: "YouTube", url: "https://www.youtube.com" },
-  ],
+  social: [{ name: "Facebook", url: "https://www.facebook.com/share/g/1DR9BF1avp/" }],
 } as const;
